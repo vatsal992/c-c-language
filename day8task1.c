d2ba680b9259554fa261ea655f6d2c36ad573d59
vatsal992 <134709444+vatsal992@users.noreply.github.com>
2023-05-31 06:14:24 -0700

    Add files via upload

---
+++ day8task1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b;
+	printf("enter value of a");
+	scanf("%d",&a);
+	if(a<b){
+	printf("value of a is minimum");
+	}
+	printf("enter value of b");
+	scanf("%d",&b);
+	if(a>b){
+	printf("value of b is minimum");
+	
+	}
+	return 0;
+	
+	
+}
---
+++ day8task2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b=0;
+	printf("enter value of a");
+	scanf("%d",&a);
+	if(a>b){
+		printf("value of a is positive");
+		
+}
+if(a<b){
+	printf("value is negative");
+	
+}
+if(a=b){
+	printf("value is netural");
+}
+    
+  
+	return 0;
+}
